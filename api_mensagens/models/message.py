e5e9d09c9c21a05fbb6482e2d3afc7845e783142
lucas-ofc <alphaz0148@gmail.com>
2025-06-18 00:12:03 -0300

    inicio

--- api_mensagens/models/message.py
+++ api_mensagens/models/message.py
@@ -1,9 +1,9 @@
 from datetime import datetime
 
-from sqlalchemy import func
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import func, ForeignKey
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 from api_mensagens.db.base import table_registry
-
+from .user import User
 
 @table_registry.mapped_as_dataclass
 class Message:
@@ -13,3 +13,5 @@ class Message:
     created_at: Mapped[datetime] = mapped_column(
         init=False, nullable=False, server_default=func.now()
     )
+    user_id: Mapped[int] = mapped_column(ForeignKey("user.id"), nullable = False)
+    user: Mapped["User"] = relationship(back_populates = "messages")
--- api_mensagens/models/user.py
+++ api_mensagens/models/user.py
@@ -3,7 +3,8 @@ from datetime import datetime
 from sqlalchemy import func
 
 from api_mensagens.db.base import table_registry
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+from typing import List
 
 
 @table_registry.mapped_as_dataclass
@@ -14,3 +15,5 @@ class User:
     email: Mapped[str] = mapped_column(nullable=False, unique=True)
     password: Mapped[str] = mapped_column(nullable=False)
     created_at: Mapped[datetime] = mapped_column(init=False, server_default=func.now())
+
+    messages: Mapped[List["Message"]] = relationship(back_populates = "user")
