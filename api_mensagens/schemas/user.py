c94c3284b6328b60c42a9b2ba31f04f77d8bec6e
Davi Lucas <davibezerra123457@gmail.com>
2025-08-11 08:34:19 -0300

    fix: changes password to meet apidoc

--- api_mensagens/schemas/user.py
+++ api_mensagens/schemas/user.py
@@ -15,30 +15,30 @@ class UserPublic(BaseModel):
 class UserCreate(BaseModel):
     nome: str
     email: EmailStr
-    senha: str = Field(..., min_length=8, examples=["Ast4!._666"])
+    senha: str = Field(..., min_length=6, examples=["Ast4!._666"])
     perfil: Optional[str] = "USUARIO"
 
-    @field_validator("senha", mode="plain")
-    @classmethod
-    def check_password(cls, value: str):
-        patterns = {
-            "uppercase letter": r"[A-Z]",
-            "lowercase letter": r"[a-z]",
-            "digit": r"\d",
-            "special character": r"[^a-zA-Z0-9]",
-        }
-
-        missing = [
-            name
-            for name, pattern in patterns.items()
-            if not re.search(pattern, value)
-        ]
-
-        if missing:
-            raise ValueError(
-                "Your password must contain a uppercase letter, lowercase letter, digit, special character and be at least 8 characters long."
-            )
-        return value
+    # @field_validator("senha", mode="plain")
+    # @classmethod
+    # def check_password(cls, value: str):
+    #     patterns = {
+    #         "uppercase letter": r"[A-Z]",
+    #         "lowercase letter": r"[a-z]",
+    #         "digit": r"\d",
+    #         "special character": r"[^a-zA-Z0-9]",
+    #     }
+    #
+    #     missing = [
+    #         name
+    #         for name, pattern in patterns.items()
+    #         if not re.search(pattern, value)
+    #     ]
+    #
+    #     if missing:
+    #         raise ValueError(
+    #             "Your password must contain a uppercase letter, lowercase letter, digit, special character and be at least 8 characters long."
+    #         )
+    #     return value
 
 
 class UserUpdate(BaseModel):
